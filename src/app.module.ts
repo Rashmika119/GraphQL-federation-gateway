0e6fc838487d4681dcc09f7bbb02951498db462b
Rashmika119 <rashmikanethsarni119@gmail.com>
2025-10-31 06:07:32 +0530

    create federation gateway

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { IntrospectAndCompose } from "@apollo/gateway";
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'vehicle', url: 'http://localhost:3000/graphql' },
+            { name: 'vehicleReport', url: 'http://localhost:4000/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
